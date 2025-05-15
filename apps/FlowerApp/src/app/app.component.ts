a4463eacf708f5f5679f82ff04c85345d5c2a1f3
emanawad14 <emanelnady2001@gmail.com>
2025-05-15 23:53:41 +0300

    first commit

---
+++ apps/FlowerApp/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  imports: [ RouterModule , ButtonModule],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'FlowerApp';
+}
