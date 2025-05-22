eda26485078216cfbe280ea3e7c48cba56434204
FatmaElhosary <engfatmafathy@gmail.com>
2025-05-22 13:05:03 +0300

    fix-carousol in large screens

--- apps/FlowerApp/src/app/shared/components/ui/carousol.component.ts
+++ apps/FlowerApp/src/app/shared/components/ui/carousol.component.ts
@@ -13,36 +13,35 @@ import { PrimaryBtnComponent } from "./primary-btn.component";
 export class CarousolComponent {
   labelName = signal('Shop Now');
 //showIndicators = window.innerWidth >= 1024;
-
-
-  responsiveOptions: any[] = [
-      {
-            breakpoint: '1500px',
-             showIndicators:true
-        },
-        {
-            breakpoint: '1024px',
-              showIndicators:true
-        },
-        {
-            breakpoint: '768px',
-             showIndicators:true
-        },
-        {
-            breakpoint: '560px',
-             showIndicators:false
-        },
-        {
-            breakpoint: '1300px',
-            showIndicators:true
-        },
-        {
-            breakpoint: '575px',
-            showIndicators:false
-        }
-    ];
   giftsList = [
     {
+      itemImageSrc: '/images/slide1.jpg',
+      thumbnailImageSrc:
+        'https://primefaces.org/cdn/primeng/images/galleria/galleria1s.jpg',
+      alt: 'Culpa ducimus nesciunt aliquam non rerum esse recusandae omnis. Rerum optio dolores et.',
+      title: 'Best Gift Shop',
+      subTitle: 'Choose Perfect gifts From Us',
+    },
+    {
+      itemImageSrc: '/images/slide2.jpg',
+      alt: 'Culpa ducimus nesciunt aliquam non rerum esse recusandae omnis. Rerum optio dolores et.',
+      title: 'Best Gift Shop',
+      subTitle: 'Choose Perfectfts From Us',
+    },
+    {
+      itemImageSrc: '/images/slide3.jpg',
+      alt: 'Culpa ducimus nesciunt aliquam non rerum esse recusandae omnis. Rerum optio dolores et.',
+      title: 'Best Gift Shop',
+      subTitle: 'Choose Perfectfts From Us',
+    },
+    {
+      itemImageSrc: '/images/slide4.jpg',
+
+      alt: 'Culpa ducimus nesciunt aliquam non rerum esse recusandae omnis. Rerum optio dolores et.',
+      title: 'Best Gift Shop',
+      subTitle: 'Choose Perfectfts From Us',
+    },
+     {
       itemImageSrc: '/images/slide1.jpg',
       thumbnailImageSrc:
         'https://primefaces.org/cdn/primeng/images/galleria/galleria1s.jpg',
