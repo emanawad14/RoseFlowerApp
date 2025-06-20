981660dd448f6ba433734c3eafa949eac4ec6c49
FatmaElhosary <engfatmafathy@gmail.com>
2025-06-21 02:22:04 +0300

    init-branch

--- apps/FlowerApp/src/app/app.routes.ts
+++ apps/FlowerApp/src/app/app.routes.ts
@@ -1,10 +1,19 @@
 import { Route } from '@angular/router';
 
-
-
 export const appRoutes: Route[] = [
-
-    {path:'' , redirectTo:'home' , pathMatch:'full'    },
-  {path:'home' , loadComponent:()=>import('./features/pages/home/home.component').then((c)=>c.HomeComponent)}
-    
+  { path: '', redirectTo: 'product-list', pathMatch: 'full' },
+   {
+    path: 'product-list',
+    loadComponent: () =>
+      import('./features/pages/productList.component').then(
+        (c) => c.ProductListComponent
+      ),
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./features/pages/home/home.component').then(
+        (c) => c.HomeComponent
+      ),
+  },
 ];
---
+++ apps/FlowerApp/src/app/features/pages/productList.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [CommonModule],
+  templateUrl: './productList.component.html',
+  styleUrl: './productList.component.scss',
+})
+export class ProductListComponent {}
