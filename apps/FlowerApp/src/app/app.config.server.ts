ca148e35fee9239a446d8bcf7f069815ca5a03bd
FatmaElhosary <engfatmafathy@gmail.com>
2025-06-22 18:03:38 +0300

    to "@angular/ssr": "~19.2.0",

--- apps/FlowerApp/src/app/app.config.server.ts
+++ apps/FlowerApp/src/app/app.config.server.ts
@@ -2,9 +2,10 @@ import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
  import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
+import { provideServerRouting } from '@angular/ssr';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering() ],
+  providers: [provideServerRendering(), provideServerRouting(serverRoutes) ],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
