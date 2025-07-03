dc557d40e8a18ca940b287e4b283247e4bf3e72c
FatmaElhosary <engfatmafathy@gmail.com>
2025-07-03 22:05:19 +0300

    fix features&about alignment in arabic

--- apps/FlowerApp/src/app/shared/components/ui/avatar-features.component.ts
+++ apps/FlowerApp/src/app/shared/components/ui/avatar-features.component.ts
@@ -4,12 +4,17 @@ import { CommonModule } from '@angular/common';
 import { AvatarModule } from 'primeng/avatar';
 import { AvatarGroupModule } from 'primeng/avatargroup';
 import { Feature } from '../../interfaces/feature';
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-avatar-features',
-  imports: [CommonModule,AvatarModule,AvatarGroupModule],
+  imports: [CommonModule, AvatarModule, AvatarGroupModule],
   templateUrl: './avatar-features.component.html',
   styleUrl: './avatar-features.component.scss',
 })
 export class AvatarFeaturesComponent {
-  @Input({required:true}) feature!:Feature;
+  @Input({ required: true }) feature!: Feature;
+  currentLang: string = 'en';
+  constructor(public _TranslateService: TranslateService) {
+    this.currentLang = this._TranslateService.currentLang;
+  }
 }
