911addd186e1106e32505ecb78e7713ace419cce
FatmaElhosary <engfatmafathy@gmail.com>
2025-07-03 19:29:35 +0300

    fixed carousol

--- apps/FlowerApp/src/app/shared/components/ui/carousol.component.ts
+++ apps/FlowerApp/src/app/shared/components/ui/carousol.component.ts
@@ -75,6 +75,34 @@ export class CarousolComponent implements OnDestroy {
             subTitle: translations['GiftSlides.SubTitle4'],
             description: translations['GiftSlides.Description'],
           },
+            {
+            id: '1',
+            bgImg: '/images/slide1.jpg',
+            title: translations['GiftSlides.Title1'],
+            subTitle: translations['GiftSlides.SubTitle1'],
+            description: translations['GiftSlides.Description'],
+          },
+          {
+            id: '2',
+            bgImg: '/images/slide2.jpg',
+            title: translations['GiftSlides.Title2'],
+            subTitle: translations['GiftSlides.SubTitle2'],
+            description: translations['GiftSlides.Description'],
+          },
+          {
+            id: '3',
+            bgImg: '/images/slide3.jpg',
+            title: translations['GiftSlides.Title3'],
+            subTitle: translations['GiftSlides.SubTitle3'],
+            description: translations['GiftSlides.Description'],
+          },
+          {
+            id: '4',
+            bgImg: '/images/slide4.jpg',
+            title: translations['GiftSlides.Title4'],
+            subTitle: translations['GiftSlides.SubTitle4'],
+            description: translations['GiftSlides.Description'],
+          },
         ];
       });
   }
